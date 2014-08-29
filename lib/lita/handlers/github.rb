1722a19e0e346c7a4965326455912715233577ed
Tim Heckman <tim@pagerduty.com>
2014-08-28 22:55:44 -0700

    remove useless require in main Github handler

--- lib/lita/handlers/github.rb
+++ lib/lita/handlers/github.rb
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'octokit'
 require 'lita-github/version'
 require 'lita-github/r'
 require 'lita-github/config'
