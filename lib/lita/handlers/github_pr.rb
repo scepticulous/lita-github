fc9c28a91658ecd4750dd8d0d7f0e0dc341980c3
Tim Heckman <tim@pagerduty.com>
2014-09-02 09:33:41 -0700

    add info command for specific pull requests
    This adds a command for pulling down information about a pull request. This a read-only operation so no function disabling is available.
    
    * `gh pr info #42 PagerDuty/lita-github`
    
    Some functionality was also split up to make the code reusable.

--- lib/lita/handlers/github_pr.rb
+++ lib/lita/handlers/github_pr.rb
@@ -36,6 +36,13 @@ module Lita
       on :loaded, :setup_octo # from LitaGithub::Octo
 
       route(
+        /#{LitaGithub::R::A_REG}pr\s+?info\s+?#(?<pr>\d+?)\s+?#{LitaGithub::R::REPO_REGEX}/,
+        :pr_info,
+        command: true,
+        help: { 'gh pr info #42 PagerDuty/lita-github' => 'show some information about the pull request' }
+      )
+
+      route(
         /(?:#{LitaGithub::R::A_REG}(?:pr merge|shipit)|shipit)\s+?#(?<pr>\d+?)\s+?#{LitaGithub::R::REPO_REGEX}/,
         :pr_merge, command: true, confirmation: true,
         help: {
@@ -45,18 +52,36 @@ module Lita
         }
       )
 
+      # rubocop:disable Metrics/CyclomaticComplexity
+      # rubocop:disable Metrics/PerceivedComplexity
+      def pr_info(response)
+        org, repo, pr = pr_match(response)
+        full_name = rpo(org, repo)
+
+        pr_h = pull_request(full_name, pr)
+        return response.reply(t('not_found', pr: pr, org: org, repo: repo)) if pr_h[:fail] && pr_h[:not_found]
+
+        info = build_pr_info(pr_h[:pr], full_name)
+        comparison_url = "https://github.com/#{full_name}/compare/#{info[:base_sha]}...#{info[:pr_sha]}"
+        info.merge!(repo: full_name, compare: comparison_url)
+
+        reply = t('pr_info.header', info) << t('pr_info.status', info)
+        reply << (info[:state] == :merged ? t('pr_info.merged', info) : t('pr_info.mergeable', info))
+        reply << t('pr_info.commit_info', info) << t('pr_info.comments', info)
+
+        response.reply(reply)
+      end
+
       def pr_merge(response)
         return response.reply(t('method_disabled')) if func_disabled?(__method__)
         org, repo, pr = pr_match(response)
+        fullname = rpo(org, repo)
 
-        begin
-          pr_obj = octo.pull_request(rpo(org, repo), pr)
-        rescue Octokit::NotFound
-          return response.reply(t('not_found', pr: pr, org: org, repo: repo))
-        end
+        pr_h = pull_request(fullname, pr)
+        return response.reply(t('not_found', pr: pr, org: org, repo: repo)) if pr_h[:fail] && pr_h[:not_found]
 
-        branch = pr_obj[:head][:ref]
-        title = pr_obj[:title]
+        branch = pr_h[:pr][:head][:ref]
+        title = pr_h[:pr][:title]
         commit = "Merge pull request ##{pr} from #{org}/#{branch}\n\n#{title}"
 
         status = merge_pr(org, repo, pr, commit)
@@ -70,12 +95,37 @@ module Lita
           response.reply(t('pr_merge.fail', pr: pr, title: title, url: url, msg: status[:message]))
         end
       end
+      # rubocop:enable Metrics/CyclomaticComplexity
+      # rubocop:enable Metrics/PerceivedComplexity
 
       private
 
       def pr_match(response)
         md = response.match_data
         [organization(md['org']), md['repo'], md['pr']]
+      end
+
+      def pull_request(full_name, pr_num)
+        ret = { fail: false, not_found: false }
+        begin
+          ret[:pr] = octo.pull_request(full_name, pr_num)
+        rescue Octokit::NotFound
+          ret[:fail] = true
+          ret[:not_found] = true
+        end
+        ret
+      end
+
+      def build_pr_info(pr_obj, full_name)
+        info = {}
+
+        build_pr_header!(info, pr_obj)
+        build_pr_commitinfo!(info, pr_obj)
+        build_pr_status!(info, pr_obj, full_name)
+        build_pr_merge!(info, pr_obj)
+        build_pr_comments!(info, pr_obj)
+
+        info
       end
 
       def merge_pr(org, repo, pr, commit)
@@ -88,6 +138,50 @@ module Lita
         end
         # rubocop:enable Lint/HandleExceptions
         status
+      end
+
+      def build_pr_header!(info, pr_obj)
+        info[:title]            = pr_obj[:title]
+        info[:number]           = pr_obj[:number]
+        info[:url]              = pr_obj[:html_url]
+        info
+      end
+
+      def build_pr_commitinfo!(info, pr_obj)
+        info[:commits]          = pr_obj[:commits]
+        info[:plus]             = pr_obj[:additions]
+        info[:minus]            = pr_obj[:deletions]
+        info[:changed_files]    = pr_obj[:changed_files]
+        info[:pr_sha]           = pr_obj[:head][:sha]
+        info[:base_sha]         = pr_obj[:base][:sha]
+        info[:pr_sha_short]     = info[:pr_sha].slice(0, 7)
+        info
+      end
+
+      def build_pr_status!(info, pr_obj, full_name)
+        user = octo.user(pr_obj[:user][:login])
+        info[:user]             = pr_obj[:user][:login]
+        info[:user]             << " (#{user[:name]})" if user.key?(:name)
+        info[:state]            = pr_obj[:merged] ? :merged : pr_obj[:state].to_sym
+        info[:state_str]        = pr_obj[:merged] ? 'Merged' : pr_obj[:state].capitalize
+        info[:build_status]     = octo.combined_status(full_name, info[:pr_sha])[:state]
+        info
+      end
+
+      def build_pr_merge!(info, pr_obj)
+        info[:mergeable]        = pr_obj[:mergeable]
+        if info[:state] == :merged
+          merger = octo.user(pr_obj[:merged_by][:login])
+          info[:merged_by] = pr_obj[:merged_by][:login]
+          info[:merged_by] << " (#{merger[:name]})" if merger.key?(:name)
+        end
+        info
+      end
+
+      def build_pr_comments!(info, pr_obj)
+        info[:review_comments]  = pr_obj[:review_comments]
+        info[:comments]         = pr_obj[:comments]
+        info
       end
     end
 
--- spec/unit/lita/handlers/github_pr_spec.rb
+++ spec/unit/lita/handlers/github_pr_spec.rb
@@ -10,8 +10,12 @@ describe Lita::Handlers::GithubPR, lita_handler: true do
   let(:github_pr) { Lita::Handlers::GithubPR.new('robot') }
   let(:github_org) { 'GrapeDuty' }
   let(:github_repo) { 'lita-test' }
+  let(:full_name) { "#{github_org}/#{github_repo}" }
   let(:disabled_reply) { 'Sorry, this function has either been disabled or not enabled in the config' }
 
+  ####
+  # Helper Methods
+  ####
   describe '.pr_match' do
     it 'should return the content of the match data' do
       mock_md = { 'org' => github_org, 'repo' => github_repo, 'pr' => 42 }
@@ -20,6 +24,351 @@ describe Lita::Handlers::GithubPR, lita_handler: true do
     end
   end
 
+  describe '.merge_pr' do
+    before do
+      @octo_obj = double('Octokit::Client', merge_pull_request: :ohai)
+      allow(github_pr).to receive(:octo).and_return(@octo_obj)
+    end
+
+    let(:pr_num) { 42 }
+    let(:ref) { '1234567890' }
+
+    context 'when all goes to plan' do
+      it 'should call octo.merge_pull_request with the proper args' do
+        expect(@octo_obj).to receive(:merge_pull_request).with(full_name, pr_num, ref)
+          .and_return(:ohai)
+        github_pr.send(:merge_pr, github_org, github_repo, pr_num, ref)
+      end
+
+      it 'should return the return of octo.merge_pull_request' do
+        x = github_pr.send(:merge_pr, github_org, github_repo, 42, '1234567890')
+        expect(x).to eql :ohai
+      end
+    end
+
+    context 'when the merging throws an exception' do
+      before do
+        allow(@octo_obj).to receive(:merge_pull_request).with(full_name, pr_num, ref)
+          .and_raise(StandardError.new)
+      end
+
+      it 'should return nil' do
+        expect(github_pr.send(:merge_pr, github_org, github_repo, pr_num, ref)).to be_nil
+      end
+    end
+  end
+
+  describe '.build_pr_header!' do
+    let(:pr_obj) do
+      { title: 'abc123', number: 42, html_url: 'https://github.com/' }
+    end
+
+    it 'should return a Hash' do
+      info = {}
+      x = github_pr.send(:build_pr_header!, info, pr_obj)
+      expect(x).to be_an_instance_of Hash
+    end
+
+    it 'should set the :title key' do
+      info = {}
+      github_pr.send(:build_pr_header!, info, pr_obj)
+      expect(info[:title]).to eql pr_obj[:title]
+    end
+
+    it 'should set the :number key' do
+      info = {}
+      github_pr.send(:build_pr_header!, info, pr_obj)
+      expect(info[:number]).to eql pr_obj[:number]
+    end
+
+    it 'should set the :url key' do
+      info = {}
+      github_pr.send(:build_pr_header!, info, pr_obj)
+      expect(info[:url]).to eql pr_obj[:html_url]
+    end
+  end
+
+  describe '.build_pr_commitinfo!' do
+    let(:pr_obj) do
+      {
+        commits: 1, additions: 4, deletions: 2, changed_files: 1,
+        head: { sha: '1234567890' }, base: { sha: '0987654321' }
+      }
+    end
+    let(:info) { {} }
+
+    it 'should return a Hash' do
+      expect(github_pr.send(:build_pr_commitinfo!, info, pr_obj)).to be_an_instance_of Hash
+    end
+
+    it 'should set the :commits key' do
+      github_pr.send(:build_pr_commitinfo!, info, pr_obj)
+      expect(info[:commits]).to eql pr_obj[:commits]
+    end
+
+    it 'should set the :plus key' do
+      github_pr.send(:build_pr_commitinfo!, info, pr_obj)
+      expect(info[:plus]).to eql pr_obj[:additions]
+    end
+
+    it 'should set the :minus key' do
+      github_pr.send(:build_pr_commitinfo!, info, pr_obj)
+      expect(info[:minus]).to eql pr_obj[:deletions]
+    end
+
+    it 'should set the :changed_files key' do
+      github_pr.send(:build_pr_commitinfo!, info, pr_obj)
+      expect(info[:changed_files]).to eql pr_obj[:changed_files]
+    end
+
+    it 'should set the :pr_sha key' do
+      github_pr.send(:build_pr_commitinfo!, info, pr_obj)
+      expect(info[:pr_sha]).to eql pr_obj[:head][:sha]
+    end
+
+    it 'should be set the :base_sha key' do
+      github_pr.send(:build_pr_commitinfo!, info, pr_obj)
+      expect(info[:base_sha]).to eql pr_obj[:base][:sha]
+    end
+
+    it 'should set the :pr_sha_short key' do
+      github_pr.send(:build_pr_commitinfo!, info, pr_obj)
+      expect(info[:pr_sha_short]).to eql info[:pr_sha].slice(0, 7)
+    end
+  end
+
+  describe '.build_pr_status!' do
+    before do
+      @user_obj = { name: 'Tim Heckman' }
+      @cs_obj = { state: 'success' }
+      @octo_obj = double('Octokit::Client', user: @user_obj, combined_status: @cs_obj)
+      allow(github_pr).to receive(:octo).and_return(@octo_obj)
+    end
+
+    let(:pr_obj) do
+      {
+        user: { login: 'theckman' }, state: 'closed', merged: true, pr_sha: '1234567890'
+      }
+    end
+    let(:info) { { pr_sha: '1234567890' } }
+
+    it 'should return an instance of Hash' do
+      expect(github_pr.send(:build_pr_status!, info, pr_obj, full_name))
+        .to be_an_instance_of Hash
+    end
+
+    it 'should call octo.user' do
+      expect(@octo_obj).to receive(:user).with('theckman')
+      github_pr.send(:build_pr_status!, info, pr_obj, full_name)
+    end
+
+    it 'should call octo.combined_status and set it to :build_status' do
+      expect(@octo_obj).to receive(:combined_status)
+        .with(full_name, '1234567890')
+        .and_return(@cs_obj)
+      github_pr.send(:build_pr_status!, info, pr_obj, full_name)
+      expect(info[:build_status]).to eql 'success'
+    end
+
+    context 'when user has a name set' do
+      it 'should include the name' do
+        github_pr.send(:build_pr_status!, info, pr_obj, full_name)
+        expect(info[:user]).to eql 'theckman (Tim Heckman)'
+      end
+    end
+
+    context 'when the PR has been merged' do
+      it 'should set the :state key to :merged' do
+        github_pr.send(:build_pr_status!, info, pr_obj, full_name)
+        expect(info[:state]).to eql :merged
+      end
+
+      it 'should set the :state_str to "Merged"' do
+        github_pr.send(:build_pr_status!, info, pr_obj, full_name)
+        expect(info[:state_str]).to eql 'Merged'
+      end
+    end
+
+    context 'when user has no name set' do
+      before do
+        @octo_obj = double('Octokit::Client', user: {}, combined_status: @cs_obj)
+        allow(github_pr).to receive(:octo).and_return(@octo_obj)
+      end
+
+      it 'should not include the real name parenthesis' do
+        github_pr.send(:build_pr_status!, info, pr_obj, full_name)
+        expect(info[:user]).to eql 'theckman'
+      end
+    end
+
+    context 'when PR not merged' do
+      context 'when status is open' do
+        let(:pr_obj) do
+          {
+            user: { login: 'theckman' }, state: 'open', merged: false
+          }
+        end
+
+        it 'should set the :state key to :open' do
+          github_pr.send(:build_pr_status!, info, pr_obj, full_name)
+          expect(info[:state]).to eql :open
+        end
+
+        it 'should set the :state_str to "Open"' do
+          github_pr.send(:build_pr_status!, info, pr_obj, full_name)
+          expect(info[:state_str]).to eql 'Open'
+        end
+      end
+
+      context 'when status is closed' do
+        let(:pr_obj) do
+          {
+            user: { login: 'theckman' }, state: 'closed', merged: false
+          }
+        end
+
+        it 'should set the :state key to :closed' do
+          github_pr.send(:build_pr_status!, info, pr_obj, full_name)
+          expect(info[:state]).to eql :closed
+        end
+
+        it 'should set the :state_str to "Closed"' do
+          github_pr.send(:build_pr_status!, info, pr_obj, full_name)
+          expect(info[:state_str]).to eql 'Closed'
+        end
+      end
+    end
+  end
+
+  describe '.build_pr_merge!' do
+    before do
+      @user_obj = { name: 'Tim Heckman' }
+      @octo_obj = double('Octokit::Client', user: @user_obj)
+      allow(github_pr).to receive(:octo).and_return(@octo_obj)
+    end
+
+    let(:pr_obj) do
+      {
+        state: :open, mergeable: true
+      }
+    end
+    let(:info) { { state: :open } }
+
+    it 'should return an instance of Hash' do
+      expect(github_pr.send(:build_pr_merge!, info, pr_obj))
+        .to be_an_instance_of Hash
+    end
+
+    it 'should set the :mergable key' do
+      github_pr.send(:build_pr_merge!, info, pr_obj)
+      expect(info[:mergeable]).to eql true
+    end
+
+    context 'when not merged' do
+      it 'should not set the :merged_by key' do
+        github_pr.send(:build_pr_merge!, info, pr_obj)
+        expect(info.key?(:merged_by)).to be_falsey
+      end
+    end
+
+    context 'when merged' do
+      let(:pr_obj) do
+        {
+          state: :merged, mergeable: nil, merged_by: { login: 'theckman' }
+        }
+      end
+      let(:info) { { state: :merged } }
+
+      it 'should grab some user info about who merged' do
+        expect(@octo_obj).to receive(:user).with('theckman').and_return(@user_obj)
+        github_pr.send(:build_pr_merge!, info, pr_obj)
+      end
+
+      context 'when user has a name field' do
+        it 'should set the :merged_by key' do
+          github_pr.send(:build_pr_merge!, info, pr_obj)
+          expect(info[:merged_by]).to eql 'theckman (Tim Heckman)'
+        end
+      end
+
+      context 'when the user has no name field' do
+        before do
+          @user_obj = {}
+          @octo_obj = double('Octokit::Client', user: @user_obj)
+          allow(github_pr).to receive(:octo).and_return(@octo_obj)
+        end
+
+        it 'should set the :merged_by key without parenthesis' do
+          github_pr.send(:build_pr_merge!, info, pr_obj)
+          expect(info[:merged_by]).to eql 'theckman'
+        end
+      end
+    end
+  end
+
+  describe '.build_pr_comments!' do
+    let(:pr_obj) { { comments: 1, review_comments: 3 } }
+    let(:info) { {} }
+
+    it 'should return a Hash' do
+      expect(github_pr.send(:build_pr_comments!, info, pr_obj)).to be_an_instance_of Hash
+    end
+
+    it 'should set the :comments key' do
+      github_pr.send(:build_pr_comments!, info, pr_obj)
+      expect(info[:comments]).to eql 1
+    end
+
+    it 'should set the :review_comments key' do
+      github_pr.send(:build_pr_comments!, info, pr_obj)
+      expect(info[:review_comments]).to eql 3
+    end
+  end
+
+  describe '.build_pr_info' do
+    before do
+      allow(github_pr).to receive(:build_pr_header!).and_return(nil)
+      allow(github_pr).to receive(:build_pr_commitinfo!).and_return(nil)
+      allow(github_pr).to receive(:build_pr_status!).and_return(nil)
+      allow(github_pr).to receive(:build_pr_merge!).and_return(nil)
+      allow(github_pr).to receive(:build_pr_comments!).and_return(nil)
+    end
+
+    let(:pr_obj) { :ohai }
+
+    it 'should return an instance of Hash' do
+      expect(github_pr.send(:build_pr_info, pr_obj, full_name)).to be_an_instance_of Hash
+    end
+
+    it 'should call .build_pr_header!' do
+      expect(github_pr).to receive(:build_pr_header!).with({}, pr_obj).and_return(nil)
+      github_pr.send(:build_pr_info, pr_obj, full_name)
+    end
+
+    it 'should call .build_pr_commitinfo!' do
+      expect(github_pr).to receive(:build_pr_commitinfo!).with({}, pr_obj).and_return(nil)
+      github_pr.send(:build_pr_info, pr_obj, full_name)
+    end
+
+    it 'should call .build_pr_status!' do
+      expect(github_pr).to receive(:build_pr_status!).with({}, pr_obj, full_name).and_return(nil)
+      github_pr.send(:build_pr_info, pr_obj, full_name)
+    end
+
+    it 'should call .build_pr_merge!' do
+      expect(github_pr).to receive(:build_pr_merge!).with({}, pr_obj).and_return(nil)
+      github_pr.send(:build_pr_info, pr_obj, full_name)
+    end
+
+    it 'should call .build_pr_comments!' do
+      expect(github_pr).to receive(:build_pr_comments!).with({}, pr_obj).and_return(nil)
+      github_pr.send(:build_pr_info, pr_obj, full_name)
+    end
+  end
+
+  ####
+  # Handlers
+  ####
   describe '.merge_pr' do
     before do
       @merge_status = { sha: 'abc456', merged: true, message: 'Pull Request successfully merged' }
@@ -49,6 +398,75 @@ describe Lita::Handlers::GithubPR, lita_handler: true do
     end
   end
 
+  describe '.pr_info' do
+    context 'when PR is not merged' do
+      before do
+        @pr_info = {
+          title: 'Test Pull Request (Not Real)', number: 42,
+          url: "https://github.com/#{github_org}/#{github_repo}/pulls/42",
+          commits: 1, plus: 42, minus: 0, changed_files: 1, pr_sha: '1234567890',
+          base_sha: '0987654321', pr_sha_short: '1234567', user: 'theckman (Tim Heckman)',
+          state: :open, state_str: 'Open', build_status: 'success', mergeable: true,
+          review_comments: 2, comments: 1
+        }
+        @pr_resp = { fail: false, not_found: false, pr: @pr_info }
+        allow(github_pr).to receive(:pull_request).and_return(@pr_resp)
+        allow(github_pr).to receive(:build_pr_info).and_return(@pr_info)
+      end
+
+      it 'should reply with the expeced output' do
+        r = 'GrapeDuty/lita-test #42: Test Pull Request (Not Real) :: ' \
+              "https://github.com/GrapeDuty/lita-test/pulls/42\n" \
+              "Opened By: theckman (Tim Heckman) | State: Open | Build: success | Mergeable: true\n" \
+              'Head: 1234567 | Commits: 1 (+42/-0) :: ' \
+              "https://github.com/GrapeDuty/lita-test/compare/0987654321...1234567890\n" \
+              "PR Comments: 1 | Code Comments: 2\n"
+        send_command("gh pr info #42 #{github_org}/#{github_repo}")
+        expect(replies.last).to eql r
+      end
+    end
+
+    context 'when the PR has been merged' do
+      before do
+        @pr_info = {
+          title: 'Test Pull Request (Not Real)', number: 42,
+          url: "https://github.com/#{github_org}/#{github_repo}/pulls/42",
+          commits: 1, plus: 42, minus: 0, changed_files: 1, pr_sha: '1234567890',
+          base_sha: '0987654321', pr_sha_short: '1234567', user: 'theckman (Tim Heckman)',
+          state: :merged, state_str: 'Merged', build_status: 'success', mergeable: true,
+          merged_by: 'theckman (Tim Heckman)', review_comments: 2, comments: 1
+        }
+        @pr_resp = { fail: false, not_found: false, pr: @pr_info }
+        allow(github_pr).to receive(:pull_request).and_return(@pr_resp)
+        allow(github_pr).to receive(:build_pr_info).and_return(@pr_info)
+      end
+
+      it 'should reply with the expeced output' do
+        r = 'GrapeDuty/lita-test #42: Test Pull Request (Not Real) :: ' \
+              "https://github.com/GrapeDuty/lita-test/pulls/42\n" \
+              'Opened By: theckman (Tim Heckman) | State: Merged | Build: success | ' \
+              "Merged By: theckman (Tim Heckman)\n" \
+              'Head: 1234567 | Commits: 1 (+42/-0) :: ' \
+              "https://github.com/GrapeDuty/lita-test/compare/0987654321...1234567890\n" \
+              "PR Comments: 1 | Code Comments: 2\n"
+        send_command("gh pr info #42 #{github_org}/#{github_repo}")
+        expect(replies.last).to eql r
+      end
+    end
+
+    context 'when the PR was not found' do
+      before do
+        @pr_resp = { fail: true, not_found: true, pr: @pr_info }
+        allow(github_pr).to receive(:pull_request).and_return(@pr_resp)
+      end
+
+      it 'should reply with the not found error' do
+        send_command("gh pr info #42 #{github_org}/#{github_repo}")
+        expect(replies.last).to eql 'Pull request #42 on GrapeDuty/lita-test not found'
+      end
+    end
+  end
+
   describe '.pr_merge' do
     before do
       @cfg_obj = double('Lita::Configuration', pr_merge_enabled: true)
@@ -66,7 +484,7 @@ describe Lita::Handlers::GithubPR, lita_handler: true do
         allow(github_pr).to receive(:func_disabled?).and_return(true)
       end
 
-      it 'should no-op and say such if the command is disabled' do
+      it 'should no-op and say such' do
         send_command("shipit #42 #{github_org}/#{github_repo}")
         expect(replies.last).to eql disabled_reply
       end
