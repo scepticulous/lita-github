66bfe5a61492ef12125da4e9dbd4518b46c759af
Tristan Chong <tristanchong@gmail.com>
2015-09-16 18:19:58 -0700

    Increment version to 0.2.0

--- lib/lita-github/version.rb
+++ lib/lita-github/version.rb
@@ -21,7 +21,7 @@
 # @author Tim Heckman <tim@pagerduty.com>
 module LitaGithub
   # lita-github version
-  VERSION = '0.1.2'
+  VERSION = '0.2.0'
 
   # lita-github version split amongst different revisions
   MAJOR_VERSION, MINOR_VERSION, REVISION = VERSION.split('.').map(&:to_i)
